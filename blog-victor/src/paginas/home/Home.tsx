9f15a9b04c34f621e56efed9bc252514647f904c
VictorCostaSantos <victorcosta123vcs@gmail.com>
2022-09-19 00:52:50 -0300

    @VictorCostaSantos [atualiando e dando continuidade ao blog]

--- blog-victor/src/paginas/home/Home.tsx
+++ blog-victor/src/paginas/home/Home.tsx
@@ -1,64 +1,76 @@
-import React from 'react';
-import {Typography, Box, Grid, Button} from '@material-ui/core';
-import './Home.css';
+import React, { useEffect } from "react";
+import { Typography, Box, Grid, Button } from "@material-ui/core";
 import TabPostagem from "../../components/postagens/tabpostagem/TabPostagem";
-
-
+import ModalPostagem from "../../components/postagens/modalPostagem/ModalPostagem";
+import "./Home.css";
+import { useNavigate } from "react-router";
+import useLocalStorage from "react-use-localstorage";
 
 function Home() {
-    return (
-      <>
-        <Grid
-          container
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-          className="caixa"
-        >
-          <Grid alignItems="center" item xs={6}>
-            <Box paddingX={20}>
-              <Typography
-                variant="h3"
-                gutterBottom
-                color="textPrimary"
-                component="h3"
-                align="center"
-                style={{ color: "white", fontWeight: "bold" }}
-              >
-                Seja bem vindo(a)!
-              </Typography>
-              <Typography
-                variant="h5"
-                gutterBottom
-                color="textPrimary"
-                component="h5"
-                align="center"
-                style={{ color: "white", fontWeight: "bold" }}
-              >
-                Aqui é um lugar feito para você que gosta de ensinar e aprender.
-              </Typography>
+  let navigate = useNavigate();
+  const [token, setToken] = useLocalStorage("token");
+
+  useEffect(() => {
+    if (token == "") {
+      alert("Você precisa estar logado");
+      navigate("/login");
+    }
+  }, [token]);
+  return (
+    <>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        className="caixa"
+      >
+        <Grid alignItems="center" item xs={6}>
+          <Box paddingX={20}>
+            <Typography
+              variant="h3"
+              gutterBottom
+              color="textPrimary"
+              component="h3"
+              align="center"
+              className="titulo"
+            >
+              Seja bem vindo(a)!
+            </Typography>
+            <Typography
+              variant="h5"
+              gutterBottom
+              color="textPrimary"
+              component="h5"
+              align="center"
+              className="titulo"
+            >
+              expresse aqui os seus pensamentos e opiniões!
+            </Typography>
+          </Box>
+          <Box display="flex" justifyContent="center">
+            <Box marginRight={1}>
+              <ModalPostagem />
             </Box>
-            <Box display="flex" justifyContent="center">
-              <Box marginRight={1}></Box>
-              <Button variant="outlined" className="botao">
-                Ver Postagens
-              </Button>
-            </Box>
-          </Grid>
-          <Grid item xs={6}>
-            <img
-              src="https://stories.freepiklabs.com/api/vectors/left-hander/cuate/render?color=FF0000FF&background=complete&hide="
-              alt=""
-              width="500px"
-              height="500px"
-            />
-          </Grid>
-          <Grid xs={12} className="postagens">
-            <TabPostagem />
-          </Grid>
+            <Button variant="outlined" className="botao">
+              Ver Postagens
+            </Button>
+          </Box>
         </Grid>
-      </>
-    );
+        <Grid item xs={6}>
+          <img
+            src="https://stories.freepiklabs.com/api/vectors/left-hander/cuate/render?color=FF0000FF&background=complete&hide="
+            alt=""
+            width="500px"
+            height="500px"
+          />
+        </Grid>
+        <Grid xs={12} className="postagens">
+          <TabPostagem />
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default Home;
